e0f6f20165c12ea6d56c37d63db0e2878199dc62
germanseoane <77026503+germanseoane@users.noreply.github.com>
2021-07-08 16:17:05 -0300

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) {
-    return res.status(400).send("Invalid email or password");
+    return res.status(400).send({error: "Invalid email or password"});
   }
   const token = jwt.sign({ _id: user._id }, process.env.delavega_jwtPrivateKey);
   res.send({ email: user.email, name: user.name , token: token});
